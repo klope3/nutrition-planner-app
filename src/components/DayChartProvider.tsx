5935bb95dec4699d0a9be371e8c455501b2aa630
Josh Klope <klope3@msn.com>
2023-02-25 19:35:00 -0600

    Refactor add portion logic

--- src/components/DayChartProvider.tsx
+++ src/components/DayChartProvider.tsx
@@ -6,7 +6,12 @@ import {
   useState,
 } from "react";
 import { DB_URL, sectionsPerDay } from "../constants";
-import { deleteFromDb, fetchFromDb, postToDb } from "../fetch";
+import {
+  deleteFromDb,
+  fetchFromDb,
+  postToDb,
+  postToDbAndReturnJson,
+} from "../fetch";
 import { DayChartData, DayChartDayData } from "../types/DayChartTypes";
 
 type DayChartContext = {
@@ -54,24 +59,19 @@ export function useDayChart() {
 
   async function addPortion(fdcId: number, fractionOfServing: number) {
     //add a portionRow with the given data
-    const rowResponse = await postToDb("portionRows", {
-      fdcId,
-      fractionOfServing,
-    });
-    if (!rowResponse.ok) {
-      console.log(
-        "Adding portion FAILED: could not add new portion row: " +
-          rowResponse.status
-      );
-      return;
-    }
-    const postJson = await rowResponse.json();
+    const postJson = await postToDbAndReturnJson(
+      "portionRows",
+      {
+        fdcId,
+        fractionOfServing,
+      },
+      "Could not add new portion row"
+    );
+    if (!postJson) return;
     const portionRowId = postJson.id;
-    console.log("Added portion with id " + portionRowId);
 
     //add a dayChartDay with the correct dayId, OR skip if there already is one
     const clickedDayIndex = Math.floor(clickedSectionIndex / sectionsPerDay);
-    console.log("Trying to find day with index " + clickedDayIndex + " in db");
     const existingDay = dayChartDays.find(
       (dayChartDay) =>
         dayChartDay.indexInChart === clickedDayIndex &&
@@ -79,70 +79,49 @@ export function useDayChart() {
     );
     let dbDayId = existingDay && existingDay.id;
     if (!existingDay) {
-      const postResponse = await postToDb("dayChartDays", {
-        dayChartId: 1,
-        indexInChart: clickedDayIndex,
-      });
-      if (!postResponse.ok) {
-        console.log(
-          "Adding portion FAILED: could not add new day: " + postResponse.status
-        );
-        return;
-      }
-      const postJson = await postResponse.json();
+      const postJson = await postToDbAndReturnJson(
+        "dayChartDays",
+        {
+          dayChartId: 1,
+          indexInChart: clickedDayIndex,
+        },
+        "Could not add new day"
+      );
+      if (!postJson) return;
       dbDayId = postJson.id;
-      console.log("Created a new day with id " + dbDayId);
-    } else {
-      console.log("Adding to existing day with id " + dbDayId);
     }
 
     //add a daySection with this dayId and the selected indexInDay, OR skip if there is already one with the same values
     //(take note of the id of the daySection created, OR the daySection that already existed)
     const indexInDay = clickedSectionIndex - sectionsPerDay * clickedDayIndex;
-    console.log(
-      "Trying to find a daySection with dayId=" +
-        dbDayId +
-        " and indexInDay=" +
-        indexInDay
-    );
     const existingSection = daySections.find(
       (daySection) =>
         daySection.dayId === dbDayId && daySection.indexInDay === indexInDay
     );
     let dbSectionId = existingSection && existingSection.id;
     if (!existingSection) {
-      const postResponse = await postToDb("daySections", {
-        dayId: dbDayId,
-        indexInDay,
-      });
-      if (!postResponse.ok) {
-        console.log(
-          "Adding portion FAILED: could not add new section: " +
-            postResponse.status
-        );
-        return;
-      }
-      const postJson = await postResponse.json();
+      const postJson = await postToDbAndReturnJson(
+        "daySections",
+        {
+          dayId: dbDayId,
+          indexInDay,
+        },
+        "Could not add new section"
+      );
+      if (!postJson) return;
       dbSectionId = postJson.id;
-      console.log("Created a new section with id " + dbSectionId);
-    } else {
-      console.log("Adding to existing section with id " + dbSectionId);
     }
 
     //add a daySectionRow with daySectionId=(previously noted daySection id) and portionRowId=(id of row already added)
-    const daySectionRowResponse = await postToDb("daySectionRows", {
-      daySectionId: dbSectionId,
-      portionRowId,
-    });
-    if (!daySectionRowResponse.ok) {
-      console.log(
-        "Adding portion FAILED: could not add new daySectionRow: " +
-          daySectionRowResponse.status
-      );
-      return;
-    }
-    const daySectionRowJson = await daySectionRowResponse.json();
-    console.log("Added daySectionRow with id " + daySectionRowJson.id);
+    const daySectionRowJson = await postToDbAndReturnJson(
+      "daySectionRows",
+      {
+        daySectionId: dbSectionId,
+        portionRowId,
+      },
+      "Could not add new daySectionRow"
+    );
+    if (!daySectionRowJson) return;
 
     fetchData(setDayChartData);
   }
--- src/fetch.ts
+++ src/fetch.ts
@@ -1,5 +1,37 @@
 import { DB_URL } from "./constants";
 
+async function fetchRequestWithJson(
+  endpoint: string,
+  requestOptions: Object,
+  badResponseMessage?: string
+) {
+  const response = await fetch(`${DB_URL}/${endpoint}`, requestOptions);
+  if (!response.ok) {
+    console.error(badResponseMessage ? badResponseMessage : "Fetch FAILED");
+    return undefined;
+  }
+  const json = await response.json();
+  return json;
+}
+
+export async function postToDbAndReturnJson(
+  endpoint: string,
+  bodyData: Object,
+  badResponseMessage?: string
+) {
+  return await fetchRequestWithJson(
+    endpoint,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(bodyData),
+    },
+    badResponseMessage
+  );
+}
+
 export function fetchFromDb(endpoint: string) {
   const requestOptions = { method: "GET" };
   return fetch(`${DB_URL}/${endpoint}`, requestOptions);
