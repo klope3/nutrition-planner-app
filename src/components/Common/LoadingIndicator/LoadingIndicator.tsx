d055d040dd0e8e7177af2918b89cf824f7ff193c
Josh Klope <klope3@msn.com>
2023-03-27 16:30:15 -0500

    Add border radius to food search and loading indicator; add animation to loading indicator

--- src/components/Common/LoadingIndicator/LoadingIndicator.tsx
+++ src/components/Common/LoadingIndicator/LoadingIndicator.tsx
@@ -1,9 +1,24 @@
 import "./LoadingIndicator.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCarrot } from "@fortawesome/free-solid-svg-icons";
 
 export function LoadingIndicator() {
   return (
     <div className="modal-bg">
-      <div className="loading-indicator">Loading...</div>
+      <div className="loading-indicator">
+        <div>
+          <FontAwesomeIcon className="loading-visual" icon={faCarrot} />
+          <FontAwesomeIcon
+            className="loading-visual anim-offset-1"
+            icon={faCarrot}
+          />
+          <FontAwesomeIcon
+            className="loading-visual anim-offset-2"
+            icon={faCarrot}
+          />
+        </div>
+        Loading...
+      </div>
     </div>
   );
 }
